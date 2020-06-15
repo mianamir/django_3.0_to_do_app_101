71ecece6770fa48e721d503b4ccd905588a0c3bb
Muhammad Amir <mianamirlahore@gmail.com>
2020-06-15 23:41:26 +0500

    initial commit

---
+++ tasks/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {
+        'tasks': tasks,
+        'form': form
+    }
+    return render(request, 'tasks/list.html', context)
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {
+        'form': form
+    }
+    return render(request, 'tasks/update_task.html', context)
+
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    context = {
+        'task': task
+    }
+    return render(request, 'tasks/delete_task.html', context)
+
